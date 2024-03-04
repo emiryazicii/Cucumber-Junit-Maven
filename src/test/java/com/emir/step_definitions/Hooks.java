3c82640a76b52577a8a925c9f45464fde756a9cf
European01 <emiryaziciofficial@gmail.com>
2024-03-04 18:22:08 -0500

    Add tearDown method .(Using @After annotation from cucumber library.)

--- src/test/java/com/emir/step_definitions/Hooks.java
+++ src/test/java/com/emir/step_definitions/Hooks.java
@@ -1,5 +1,6 @@
 package com.emir.step_definitions;
 
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 public class Hooks {
@@ -7,5 +8,10 @@ public class Hooks {
     @Before
     public void setupMethod(){
         System.out.println("--->@Before: RUNNING BEFORE EACH SCENARIO");
+    }
+
+    @After
+    public void tearDown(){
+        System.out.println("---> @After: RUNNING AFTER EACH SCENARIO");
     }
 }
