f5f7716e8c645ce377b2a69bbd3ba808de2b5008
European01 <emiryaziciofficial@gmail.com>
2024-03-04 18:18:59 -0500

    Add setupMethod.(Using @Before annotation from cucumber library.)

--- src/test/java/com/emir/step_definitions/Hooks.java
+++ src/test/java/com/emir/step_definitions/Hooks.java
@@ -1,4 +1,11 @@
 package com.emir.step_definitions;
 
+import io.cucumber.java.Before;
+
 public class Hooks {
+
+    @Before
+    public void setupMethod(){
+        System.out.println("--->@Before: RUNNING BEFORE EACH SCENARIO");
+    }
 }
