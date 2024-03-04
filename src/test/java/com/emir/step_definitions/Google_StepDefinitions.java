f9172578643405663f8201755b0a3f23e326afe6
European01 <emiryaziciofficial@gmail.com>
2024-03-04 12:32:18 -0500

    Update the class.

--- src/test/java/com/emir/step_definitions/Google_StepDefinitions.java
+++ src/test/java/com/emir/step_definitions/Google_StepDefinitions.java
@@ -1,18 +1,25 @@
 package com.emir.step_definitions;
 
+import com.emir.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class Google_StepDefinitions {
 
     @When("user is on the Google search page")
     public void user_is_on_the_google_search_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+        Driver.getDriver().get("https://www.google.com");
+
     }
     @Then("user should see title is Google")
     public void user_should_see_title_is_google() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedTitle = "Google";
+
+        Assert.assertEquals(actualTitle,expectedTitle);
     }
 }
