f2f889436bbb811b1de7b3830ad4fdc05b2d2aa0
European01 <emiryaziciofficial@gmail.com>
2024-03-04 12:33:39 -0500

    Add comments and javaDoc.

--- src/test/java/com/emir/runners/CukesRunner.java
+++ src/test/java/com/emir/runners/CukesRunner.java
@@ -4,12 +4,15 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
+/**
+ * JUnit runner class for executing Cucumber tests.
+ */
+@RunWith(Cucumber.class) // Indicates that this class should be run with JUnit
 @CucumberOptions(
-
+        // Specifies the location of feature files
         features = "src/test/resources/features",
+        // Specifies the package where step definitions are located
         glue = "com/emir/step_definitions"
-
 )
 public class CukesRunner {
 }
