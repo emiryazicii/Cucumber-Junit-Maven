24cdf8c067efe1317c5aff9916cc5274d9006e9a
European01 <emiryaziciofficial@gmail.com>
2024-03-04 12:27:42 -0500

    Add step definitions.

--- src/test/java/com/emir/step_definitions/Google_StepDefinitions.java
+++ src/test/java/com/emir/step_definitions/Google_StepDefinitions.java
@@ -1,4 +1,18 @@
 package com.emir.step_definitions;
 
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class Google_StepDefinitions {
+
+    @When("user is on the Google search page")
+    public void user_is_on_the_google_search_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user should see title is Google")
+    public void user_should_see_title_is_google() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
