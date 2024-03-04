744ce21fcfce3dcc83aa27d81ed8cfbe3a7f774b
European01 <emiryaziciofficial@gmail.com>
2024-03-04 12:32:05 -0500

    Setup runner class.

--- src/test/java/com/emir/runners/CukesRunner.java
+++ src/test/java/com/emir/runners/CukesRunner.java
@@ -1,4 +1,15 @@
 package com.emir.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features = "src/test/resources/features",
+        glue = "com/emir/step_definitions"
+
+)
 public class CukesRunner {
 }
