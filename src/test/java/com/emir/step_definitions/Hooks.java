e044ee2c7d96e2f98c059626b947f27239ff4c68
European01 <emiryaziciofficial@gmail.com>
2024-03-04 18:29:19 -0500

    Add javaDoc .

--- src/test/java/com/emir/step_definitions/Hooks.java
+++ src/test/java/com/emir/step_definitions/Hooks.java
@@ -5,23 +5,38 @@ import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
 import io.cucumber.java.BeforeStep;
 
+/**
+ * This class contains Cucumber hooks for setup and teardown operations.
+ */
 public class Hooks {
 
+    /**
+     * Method to execute before each scenario.
+     */
     @Before
     public void setupMethod(){
         System.out.println("--->@Before: RUNNING BEFORE EACH SCENARIO");
     }
 
+    /**
+     * Method to execute after each scenario.
+     */
     @After
     public void tearDown(){
         System.out.println("---> @After: RUNNING AFTER EACH SCENARIO");
     }
 
+    /**
+     * Method to execute before each step within a scenario.
+     */
     @BeforeStep
     public void setupStep(){
         System.out.println("----->@BeforeStep: RUNNING BEFORE EACH STEP");
     }
 
+    /**
+     * Method to execute after each step within a scenario.
+     */
     @AfterStep
     public void tearDownStep(){
         System.out.println("------>@AfterStep: RUNNING AFTER EACH STEP");
