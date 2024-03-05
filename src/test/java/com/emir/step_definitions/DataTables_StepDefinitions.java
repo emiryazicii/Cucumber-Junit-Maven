9d81e059e64662e0b817f9d204ff0869c7892cf3
European01 <emiryaziciofficial@gmail.com>
2024-03-04 23:31:25 -0500

    Define steps definitions for the scenario.

--- src/test/java/com/emir/step_definitions/DataTables_StepDefinitions.java
+++ src/test/java/com/emir/step_definitions/DataTables_StepDefinitions.java
@@ -1,6 +1,7 @@
 package com.emir.step_definitions;
 
 import io.cucumber.java.en.Then;
+
 import java.util.List;
 
 /**
@@ -17,5 +18,16 @@ public class DataTables_StepDefinitions {
     public void user_should_see_below_list(List<String> fruitsAndVegetables) {
         // Print out the list of fruits and vegetables
         System.out.println("fruitsAndVegetables = " + fruitsAndVegetables);
+    }
+
+    /**
+     * Step definition for verifying the list of animals.
+     *
+     * @param animals The list of animals.
+     */
+    @Then("user should see below list of animals")
+    public void user_should_see_below_list_of_animals(List<String> animals) {
+        // Print out the list of animals
+        System.out.println("animals = " + animals);
     }
 }
