7f4921a0b48410a9e9cbc5f88ec198b94b65f96d
European01 <emiryaziciofficial@gmail.com>
2024-03-04 19:51:48 -0500

    Define step definitions for search feature.

--- src/test/java/com/emir/step_definitions/Bing_StepDefinitions.java
+++ src/test/java/com/emir/step_definitions/Bing_StepDefinitions.java
@@ -1,4 +1,44 @@
 package com.emir.step_definitions;
 
+import com.emir.pages.BingSearchPage;
+import com.emir.utilities.BrowserUtils;
+import com.emir.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+
+/**
+ * Step definitions class for Bing search feature.
+ */
 public class Bing_StepDefinitions {
+
+    // Initializing BingSearchPage object
+    BingSearchPage bingSearchPage = new BingSearchPage();
+
+    /**
+     * Step definition for navigating to the Bing search page.
+     */
+    @Given("user is on the Bing search page")
+    public void user_is_on_the_bing_search_page() {
+        // Navigate to Bing search page
+        Driver.getDriver().get("https://www.bing.com");
+    }
+
+    /**
+     * Step definition for searching for "orange" in Bing search box.
+     */
+    @When("user searches for orange in Bing search box")
+    public void user_searches_for_orange_in_bing_search_box() {
+        bingSearchPage.searchBox.sendKeys("orange" + Keys.ENTER);
+    }
+
+    /**
+     * Step definition for verifying if the title contains "orange".
+     */
+    @Then("user should see orange in title")
+    public void user_should_see_orange_in_title() {
+        Assert.assertTrue(BrowserUtils.verifyTitle("orange - Search"));
+    }
 }
