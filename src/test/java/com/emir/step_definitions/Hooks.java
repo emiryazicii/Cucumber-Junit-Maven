6f908aa581e0bc1f0b0805eca95a85f632a7f6c5
European01 <emiryaziciofficial@gmail.com>
2024-03-04 20:34:47 -0500

    Add a function to get and attach a screenshot.

--- src/test/java/com/emir/step_definitions/Hooks.java
+++ src/test/java/com/emir/step_definitions/Hooks.java
@@ -1,10 +1,9 @@
 package com.emir.step_definitions;
 
 import com.emir.utilities.Driver;
-import io.cucumber.java.After;
-import io.cucumber.java.AfterStep;
-import io.cucumber.java.Before;
-import io.cucumber.java.BeforeStep;
+import io.cucumber.java.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 /**
  * This class contains Cucumber hooks for setup and teardown operations.
@@ -15,33 +14,42 @@ public class Hooks {
      * Method to execute before each scenario.
      */
     @Before
-    public void setupMethod(){
+    public void setupMethod() {
         System.out.println("--->@Before: RUNNING BEFORE EACH SCENARIO");
     }
 
     /**
      * Method to execute after each scenario.
+     *
+     * @param scenario The scenario object representing the current scenario.
      */
     @After
-    public void tearDown(){
+    public void tearDown(Scenario scenario) {
+        // Take screenshot of the current state of the browser
+        byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        // Attach the screenshot to the scenario report
+        scenario.attach(screenshot, "image/png", scenario.getName());
+
         System.out.println("---> @After: RUNNING AFTER EACH SCENARIO");
+
         // Close the driver
         Driver.closeDriver();
+
     }
 
     /**
      * Method to execute before each step within a scenario.
      */
-   // @BeforeStep
-    public void setupStep(){
+    // @BeforeStep
+    public void setupStep() {
         System.out.println("----->@BeforeStep: RUNNING BEFORE EACH STEP");
     }
 
     /**
      * Method to execute after each step within a scenario.
      */
-   // @AfterStep
-    public void tearDownStep(){
+    // @AfterStep
+    public void tearDownStep() {
         System.out.println("------>@AfterStep: RUNNING AFTER EACH STEP");
     }
 }
