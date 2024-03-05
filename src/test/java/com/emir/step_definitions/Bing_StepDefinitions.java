e671ce9a6b48b7bb7979d1a59cb41e5102fc38d1
European01 <emiryaziciofficial@gmail.com>
2024-03-04 20:13:08 -0500

    Add step definitions for that particular scenario.

--- src/test/java/com/emir/step_definitions/Bing_StepDefinitions.java
+++ src/test/java/com/emir/step_definitions/Bing_StepDefinitions.java
@@ -43,4 +43,31 @@ public class Bing_StepDefinitions {
     public void user_should_see_orange_in_title() {
         Assert.assertTrue(BrowserUtils.verifyTitle("orange - Search"));
     }
+
+    /**
+     * Step definition for searching for a term in the Bing search box.
+     *
+     * @param searchKey The term to search for.
+     */
+    @When("user searches for {string} in Bing search box")
+    public void userSearchesForInBingSearchBox(String searchKey) {
+        // Entering the search term in the search box
+        bingSearchPage.searchBox.sendKeys(searchKey);
+        // Adding a delay for demonstration purposes
+        BrowserUtils.sleep(3);
+        // Pressing Enter to perform the search
+        bingSearchPage.searchBox.sendKeys(Keys.ENTER);
+    }
+
+    /**
+     * Step definition for verifying if the provided term is present in the title of the search results page.
+     *
+     * @param expectedTitle The expected title to verify.
+     */
+    @Then("user should see {string} in title")
+    public void userShouldSeeInTitle(String expectedTitle) {
+        // Verifying if the expected title is present in the actual title of the page
+        Assert.assertTrue(BrowserUtils.verifyTitle(expectedTitle));
+    }
+
 }
