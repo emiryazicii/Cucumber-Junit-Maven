42b485e64e70723405754fe293661ef4648b0aeb
European01 <emiryaziciofficial@gmail.com>
2024-03-04 23:21:13 -0500

    Define step definitions for the scenario.

--- src/test/java/com/emir/step_definitions/DataTables_StepDefinitions.java
+++ src/test/java/com/emir/step_definitions/DataTables_StepDefinitions.java
@@ -1,4 +1,21 @@
 package com.emir.step_definitions;
 
+import io.cucumber.java.en.Then;
+import java.util.List;
+
+/**
+ * Step definitions class for handling Cucumber Data Tables.
+ */
 public class DataTables_StepDefinitions {
+
+    /**
+     * Step definition for verifying the list of fruits and vegetables.
+     *
+     * @param fruitsAndVegetables The list of fruits and vegetables.
+     */
+    @Then("user should see below list")
+    public void user_should_see_below_list(List<String> fruitsAndVegetables) {
+        // Print out the list of fruits and vegetables
+        System.out.println("fruitsAndVegetables = " + fruitsAndVegetables);
+    }
 }
