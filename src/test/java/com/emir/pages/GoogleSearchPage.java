0765311d218938787c5214018d7fbec60f133174
European01 <emiryaziciofficial@gmail.com>
2024-03-04 19:05:39 -0500

    Initialize the driver with object and locate the search box.

--- src/test/java/com/emir/pages/GoogleSearchPage.java
+++ src/test/java/com/emir/pages/GoogleSearchPage.java
@@ -1,4 +1,25 @@
 package com.emir.pages;
 
+import com.emir.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * This class represents the Google Search page.
+ */
 public class GoogleSearchPage {
+
+    /**
+     * Constructor to initialize elements of the GoogleSearchPage class.
+     */
+    public GoogleSearchPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    /**
+     * WebElement representing the search box on the Google search page.
+     */
+    @FindBy(id = "APjFqb")
+    public WebElement searchBox;
 }
