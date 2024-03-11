0657e1c65651d041119f26e5430472adbae893b8
European01 <emiryaziciofficial@gmail.com>
2024-03-11 18:48:54 -0400

    Add all the codes needed to activate FailedTestRunner class.

--- src/test/java/com/emir/runners/FailedTestRunner.java
+++ src/test/java/com/emir/runners/FailedTestRunner.java
@@ -1,4 +1,15 @@
 package com.emir.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class) // Indicates that this class should be run with JUnit
+@CucumberOptions(
+        // Specifies the location of the failed test scenarios list
+        features = "@target/rerun.txt",
+        // Specifies the package where step definitions are located
+        glue= "com/emir/step_definitions"
+)
 public class FailedTestRunner {
 }
