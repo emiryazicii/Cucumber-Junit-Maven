a645fdef4c9f204c7ed351c7aaf33425a1e2adf2
European01 <emiryaziciofficial@gmail.com>
2024-03-12 14:10:15 -0400

    Define the step definitions for particular scenario.Add comments and javadoc.

--- src/test/java/com/emir/step_definitions/EtsyStepDefinitions.java
+++ src/test/java/com/emir/step_definitions/EtsyStepDefinitions.java
@@ -1,16 +1,20 @@
 package com.emir.step_definitions;
 
+import com.emir.pages.EtsyHomePage;
 import com.emir.utilities.BrowserUtils;
 import com.emir.utilities.ConfigurationReader;
 import com.emir.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.junit.Assert;
 
 /**
  * Step definitions for Etsy-related scenarios in Cucumber tests.
  */
 public class EtsyStepDefinitions {
+
+    EtsyHomePage etsyHomePage = new EtsyHomePage();
 
     /**
      * Navigates the user to the Etsy homepage.
@@ -31,4 +35,35 @@ public class EtsyStepDefinitions {
         // Asserts that the title of the webpage matches the expected title
         Assert.assertTrue(BrowserUtils.verifyTitle(ConfigurationReader.getProperty("expected.title.etsy")));
     }
+
+    /**
+     * Performs the action of typing "Wooden Spoon" in the search box on the Etsy homepage.
+     * This step is specified in the Cucumber feature files.
+     */
+    @When("user types Wooden Spoon in the search box")
+    public void user_types_wooden_spoon_in_the_search_box() {
+        // Types "Wooden Spoon" into the search box
+        etsyHomePage.searchBox.sendKeys(ConfigurationReader.getProperty("search.data.etsy"));
+    }
+
+    /**
+     * Performs the action of clicking the search button on the Etsy homepage.
+     * This step is specified in the Cucumber feature files.
+     */
+    @When("user clicks to Etsy search button")
+    public void user_clicks_to_etsy_search_button() {
+        // Clicks the search button
+        etsyHomePage.searchButton.click();
+    }
+
+    /**
+     * Verifies that "Wooden Spoon" is present in the title after performing a search on Etsy.
+     * This step is specified in the Cucumber feature files.
+     */
+    @Then("user sees Wooden Spoon is in the title")
+    public void user_sees_wooden_spoon_is_in_the_title() {
+        // Verifies that "Wooden Spoon" is in the title
+        Assert.assertTrue(BrowserUtils.verifyTitle(ConfigurationReader.getProperty("expected.title.etsy.search")));
+    }
 }
+
