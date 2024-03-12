e636d1df00556c753f9caff54d308a15b28c3ca5
European01 <emiryaziciofficial@gmail.com>
2024-03-12 15:39:30 -0400

    Initialize the driver and locate the element.Add comments and javadoc.

--- src/test/java/com/emir/pages/WebTableOrderPage.java
+++ src/test/java/com/emir/pages/WebTableOrderPage.java
@@ -1,4 +1,27 @@
 package com.emir.pages;
 
-public class WebTableOrderPage {
+import com.emir.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Page class representing the order page of the web application with a web table.
+ * Extends the WebTableBasePage class.
+ */
+public class WebTableOrderPage extends WebTableBasePage {
+
+    /**
+     * Constructor to initialize the page elements using PageFactory.
+     * It initializes the page elements using the driver instance.
+     */
+    public WebTableOrderPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    /**
+     * WebElement representing the product dropdown on the order page.
+     */
+    @FindBy(name = "product")
+    public WebElement productDropdown;
 }
