0cc397180d57b065a40c3ea5fdaf909aad375ae8
European01 <emiryaziciofficial@gmail.com>
2024-03-12 15:49:38 -0400

    Define step definitions for the particular scenario.Add javadoc and comments.

--- src/test/java/com/emir/step_definitions/WebTableStepDefinitions.java
+++ src/test/java/com/emir/step_definitions/WebTableStepDefinitions.java
@@ -1,13 +1,17 @@
 package com.emir.step_definitions;
 
 import com.emir.pages.WebTableLoginPage;
+import com.emir.pages.WebTableOrderPage;
+import com.emir.utilities.BrowserUtils;
 import com.emir.utilities.ConfigurationReader;
 import com.emir.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.support.ui.Select;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -107,4 +111,43 @@ public class WebTableStepDefinitions {
         webTableLoginPage.passwordBox.sendKeys(credentials.get("password"));
     }
 
+    // Creating an instance of the WebTableOrderPage class
+    WebTableOrderPage webTableOrderPage = new WebTableOrderPage();
+
+    /**
+     * Navigates the user to the WebTable application login page
+     * and performs login using predefined credentials.
+     * This step is specified in the Cucumber feature files.
+     */
+    @Given("user is already logged in to the WebTable app")
+    public void user_is_already_logged_in_to_the_web_table_app() {
+        // Navigates to the URL specified in the configuration file
+        Driver.getDriver().get(ConfigurationReader.getProperty("env.webTable.login"));
+        // Performs login using predefined credentials
+        webTableLoginPage.login();
+    }
+
+    /**
+     * Clicks on the Orders link to navigate to the Orders page.
+     * This step is specified in the Cucumber feature files.
+     */
+    @When("user is on the Orders page")
+    public void user_is_on_the_orders_page() {
+        // Clicks on the Orders link to navigate to the Orders page
+        webTableOrderPage.orderLink.click();
+    }
+
+    /**
+     * Verifies the options displayed under the product dropdown on the Orders page.
+     * This step is specified in the Cucumber feature files.
+     *
+     * @param expectedOptions The list of expected options under the product dropdown.
+     */
+    @Then("user sees below options under product dropdown")
+    public void user_sees_below_options_under_product_dropdown(List<String> expectedOptions) {
+        // Retrieves the actual options from the product dropdown as a list of strings
+        List<String> actualOptions = BrowserUtils.dropdownOptions_as_String(webTableOrderPage.productDropdown);
+        // Asserts that the actual options match the expected options
+        Assert.assertEquals(expectedOptions, actualOptions);
+    }
 }
