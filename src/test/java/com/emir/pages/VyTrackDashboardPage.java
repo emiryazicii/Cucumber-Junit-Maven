5f28a01cf424f9a1157e72e7de3f5fc7fd95be81
European01 <emiryaziciofficial@gmail.com>
2024-03-13 18:35:07 -0400

    Initialize the driver and locate the elements.Add custom logout method.Add javadoc.

--- src/test/java/com/emir/pages/VyTrackDashboardPage.java
+++ src/test/java/com/emir/pages/VyTrackDashboardPage.java
@@ -1,4 +1,35 @@
 package com.emir.pages;
 
+import com.emir.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * This class represents the Dashboard page of VyTrack application.
+ */
 public class VyTrackDashboardPage {
+
+    /**
+     * Constructor to initialize the VyTrackDashboardPage object.
+     */
+    public VyTrackDashboardPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    /** WebElement representing the full name displayed on the dashboard. */
+    @FindBy(xpath = "//li[@id='user-menu']/a")
+    public WebElement fullName;
+
+    /** WebElement representing the logout link on the dashboard. */
+    @FindBy(linkText = "Logout")
+    public WebElement logOutLink;
+
+    /**
+     * Performs logout action by clicking on the full name and then the logout link.
+     */
+    public void logout() {
+        fullName.click();
+        logOutLink.click();
+    }
 }
