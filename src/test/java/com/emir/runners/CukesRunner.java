312a9281ac3d2bae327c5737da3a731f5945397f
European01 <emiryaziciofficial@gmail.com>
2024-03-13 18:43:28 -0400

    Run the program for @wip tag.

--- src/test/java/com/emir/runners/CukesRunner.java
+++ src/test/java/com/emir/runners/CukesRunner.java
@@ -25,7 +25,7 @@ import org.junit.runner.RunWith;
         // Whether to execute a dry run of the feature files
         dryRun = false,
         // Specifies the tags to include/exclude specific scenarios or features
-        tags = "",
+        tags = "@wip",
         // Whether to publish the results
         publish = true
 )
