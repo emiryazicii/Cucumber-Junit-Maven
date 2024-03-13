4c88c27e771124c70f537022650c789df7aa5e00
European01 <emiryaziciofficial@gmail.com>
2024-03-13 18:03:29 -0400

    Initialize the driver and locate the elements.Create custom login method.Add comments and javadoc.

--- src/test/java/com/emir/pages/VytrackLoginPage.java
+++ src/test/java/com/emir/pages/VytrackLoginPage.java
@@ -1,4 +1,41 @@
 package com.emir.pages;
 
+import com.emir.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * This class represents the Vytrack login page.
+ */
 public class VytrackLoginPage {
+
+    /** Constructor to initialize the page elements. */
+    public VytrackLoginPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    /** WebElement representing the username input field. */
+    @FindBy(id="prependedInput")
+    public WebElement loginUser;
+
+    /** WebElement representing the password input field. */
+    @FindBy(id="prependedInput2")
+    public WebElement loginPass;
+
+    /** WebElement representing the login button. */
+    @FindBy(id="_submit")
+    public WebElement loginBtn;
+
+    /**
+     * Logs in to Vytrack with the provided username and password.
+     *
+     * @param username the username to login with.
+     * @param password the password to login with.
+     */
+    public void login(String username, String password){
+        loginUser.sendKeys(username);
+        loginPass.sendKeys(password);
+        loginBtn.click();
+    }
 }
